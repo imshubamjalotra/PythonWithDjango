794a424ec8dbf2895a20746d34e1754083d1dea4
Shubam Jalotra <shubamjalotra01952@gmail.com>
2026-06-27 19:18:04 +0530

    test code

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
 import sqlite3
 import json
 from pathlib import Path
-movies = (Path("movies.json").read_text(encoding="utf-8"))
+
+base_dir = Path(__file__).resolve().parent
+movies_path = base_dir / "movies.json"
+movies = json.loads(movies_path.read_text())
 # movies = json.loads(movies)
 
 
+with sqlite3.connect("movies1.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS movies (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            year INTEGER NOT NULL
+            
+        )
+        """
+    )
+
+    for movie in movies:
+        cursor.execute(
+            "INSERT INTO movies (title, year) VALUES (?, ?)",
+            (movie["title"], movie["year"]),
+        )
 print("Creating database and table...", movies)
